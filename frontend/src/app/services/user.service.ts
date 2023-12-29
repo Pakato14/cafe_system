5bd32284511a462ba63519f3c9ea665abf8677da
Daniel Araujo <daniel.araujo@sedet.ce.gov.br>
2023-12-28 22:20:25 -0300

    frontend

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  url = environment.apiUrl;
+  constructor(private httpClient: HttpClient) {}
+
+  signup(data: any) {
+    return this.httpClient.post(this.url + '/signup', data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    });
+  }
+
+  forgotPassword(data: any){
+    return this.httpClient.post(this.url+"/forgotPassword", data,{
+      headers: new HttpHeaders().set('Content-Type', "application/json")
+    })
+  }
+
+  login(data: any){
+    return this.httpClient.post(this.url+"/login", data,{
+      headers: new HttpHeaders().set('Content-Type', "application/json")
+    })
+  }
+}
